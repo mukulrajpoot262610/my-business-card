339e0c37f69219672fda18826c3ceafe6287d639
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2022-10-11 08:14:50 +0530

    add: business card

--- build.js
+++ build.js
@@ -15,45 +15,47 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('           Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  shorthandle: chalk.white('bnb'),
-  work: chalk.white('Staff Developer Advocate at Twilio'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
-  labelWork: chalk.white.bold('    Work:'),
-  labelTwitter: chalk.white.bold(' Twitter:'),
-  labelnpm: chalk.white.bold('     npm:'),
-  labelGitHub: chalk.white.bold('  GitHub:'),
-  labelLinkedIn: chalk.white.bold('LinkedIn:'),
-  labelWeb: chalk.white.bold('     Web:'),
-  labelCard: chalk.white.bold('    Card:')
+  name: chalk.white('              Mukul Rajpoot 😎'),
+  handle: chalk.white('mukulrajpoot262610'),
+  work: chalk.white('SDE Intern at Stratzy'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('mukul_eth'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~mukulrajpoot262610'),
+  github: chalk.gray('https://github.com/') + chalk.green('mukulrajpoot262610'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('mukulrajpoot262610'),
+  web: chalk.cyan('https://mukulrajpoot.com'),
+  email: chalk.cyan('mukulrajpoot262610@gmail.com'),
+  npx: chalk.red('npx') + ' ' + chalk.white('mukulrajpoot262610'),
+  labelWork: chalk.white.bold('    Work 💻:'),
+  labelTwitter: chalk.white.bold(' Twitter 🕊️:'),
+  labelnpm: chalk.white.bold('     npm 🦄:'),
+  labelGitHub: chalk.white.bold('  GitHub 🐧:'),
+  labelLinkedIn: chalk.white.bold('LinkedIn 🤵:'),
+  labelWeb: chalk.white.bold('     Web 🌐:'),
+  labelEmail: chalk.white.bold('     Email 📧:'),
+  labelCard: chalk.white.bold('    Card 💳:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
-const heading = `${data.name} / ${data.handle} / ${data.shorthandle}`
+const heading = `${data.name}`
 const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
+const emailing = `${data.labelEmail}  ${data.email}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+  newline + newline + // Add one whole blank line
+  working + newline + // data.labelWork + data.work
+  twittering + newline + // data.labelTwitter + data.twitter
+  npming + newline + // data.labelnpm + data.npm
+  githubing + newline + // data.labelGitHub + data.github
+  linkedining + newline + // data.labelLinkedIn + data.linkedin
+  webing + newline + newline + // data.labelWeb + data.web
+  carding // data.labelCard + data.npx
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
